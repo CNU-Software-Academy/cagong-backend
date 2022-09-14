18eef32a772f068d2f78444300b39a68ba0b2a4d
minju <mjeon7890@gmail.com>
2022-09-14 14:20:25 +0900

    Initial Commit

---
+++ src/main/java/kr/ac/cnu/swacademy/cagong/CagongApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.cnu.swacademy.cagong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CagongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CagongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/cnu/swacademy/cagong/CagongApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.cnu.swacademy.cagong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CagongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
