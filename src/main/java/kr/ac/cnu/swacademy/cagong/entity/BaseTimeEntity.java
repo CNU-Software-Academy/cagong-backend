4fcf6673ce23bd3fdc206888fc5dc99a6acfba81
moonstal1506 <moonstal1506@gmail.com>
2022-09-20 16:59:49 +0900

    feat: entity

---
+++ src/main/java/kr/ac/cnu/swacademy/cagong/entity/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package kr.ac.cnu.swacademy.cagong.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime regTime;
+
+    @LastModifiedDate
+    private LocalDateTime updateTime;
+
+}
---
+++ src/main/java/kr/ac/cnu/swacademy/cagong/entity/Review.java
@@ -0,0 +1,41 @@
+package kr.ac.cnu.swacademy.cagong.entity;
+
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@Table(name="reviews")
+@Entity
+public class Review extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "review_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cafe_id")
+    private Cafe cafe;
+
+    @NotBlank
+    @Size(min = 5, message = "최소 5글자를 입력해주세요.")
+    @Column(length = 45)
+    private String content;
+
+    private String imageUrl;
+
+    @NotNull
+    private int clean;
+    @NotNull
+    private int seat;
+    @NotNull
+    private int concentration;
+}
