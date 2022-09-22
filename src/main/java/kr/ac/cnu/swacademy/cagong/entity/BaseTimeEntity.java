ff6b20ac46934f7d3dd44ee34a253a2def9a1d48
moonstal1506 <moonstal1506@gmail.com>
2022-09-22 20:50:38 +0900

    refactor: @NotNull로 변경

--- src/main/java/kr/ac/cnu/swacademy/cagong/entity/BaseTimeEntity.java
+++ src/main/java/kr/ac/cnu/swacademy/cagong/entity/BaseTimeEntity.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Getter
@@ -15,12 +16,13 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
 
+    @NotNull
     @CreatedDate
-    @Column(updatable = false, nullable = false)
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @NotNull
     @LastModifiedDate
-    @Column(nullable = false)
     private LocalDateTime updatedAt;
 
 }
