53263cae0f6cec39d8675712cdfe66343d6b3c8c
SuJeong Moon <msj1506@naver.com>
2022-09-28 10:57:08 +0900

    feat: 공지사항 등록 (#4)
    * refactor: change BaseTimeEntity Column name
    
    * feat: add NotNull
    
    * feat: 공지사항 등록
    
    * refactor: @NotNull로 변경
    
    * chore: thymeleaf layout, devtools 의존성 추가
    
    * feat: 공지사항 등록 화면
    
    * chore: 레이아웃 삭제
    
    * refactor: 변수명 변경
    
    * chore: newline 추가
    
    * fix: 공지사항 부분 active

---
+++ src/main/java/kr/ac/cnu/swacademy/cagong/controller/api/NoticeApiController.java
@@ -0,0 +1,21 @@
+package kr.ac.cnu.swacademy.cagong.controller.api;
+
+import kr.ac.cnu.swacademy.cagong.dto.NoticeSaveRequestDto;
+import kr.ac.cnu.swacademy.cagong.service.NoticeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequiredArgsConstructor
+@RestController
+public class NoticeApiController {
+
+    private final NoticeService noticeService;
+
+    @PostMapping("/api/v1/notice")
+    public long save(@RequestBody NoticeSaveRequestDto requestDto) {
+        return noticeService.save(requestDto);
+    }
+}
