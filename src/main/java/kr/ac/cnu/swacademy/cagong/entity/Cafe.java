c5b7c51c26fa659def8397e006fd1d375f421601
Kim-AYoung <onionlily123@naver.com>
2022-10-03 23:32:56 +0900

    feat: Cafe 엔티티에 새로운 필드 추가

--- src/main/java/kr/ac/cnu/swacademy/cagong/entity/Cafe.java
+++ src/main/java/kr/ac/cnu/swacademy/cagong/entity/Cafe.java
@@ -4,9 +4,9 @@ import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 @NoArgsConstructor
 @Table(name="cafes")
@@ -28,18 +28,43 @@ public class Cafe extends BaseTimeEntity {
     @Column(length = 45)
     private String address;
 
+    @NotNull
+    private Double longitude;
+
+    @NotNull
+    private Double latitude;
+
+    private Double averagePrice;
+
+    /**
+     * 네이버에서의 평점
+     */
+    private Double averageScoreInNaver;
+
+    /**
+     * 네이버에서 좌석이 좋아요를 선택한 사용자의 수
+     */
+    private Integer seatSelectionCountInNaver;
+
+    /**
+     * 네이버에서 집중하기 좋아요를 선택한 사용자의 수
+     */
+    private Integer concentrationSelectionCountInNaver;
+
     @OneToMany(mappedBy = "cafe", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Review> reviews = new ArrayList<>();
 
     @Builder
-    public Cafe(String name, String address) {
+    public Cafe(String name, String address, Double averagePrice, Double longitude, Double latitude, Double averageScoreInNaver, Integer seatSelectionCountInNaver, Integer concentrationSelectionCountInNaver) {
         this.name = name;
         this.address = address;
+        this.averagePrice = averagePrice;
+        this.longitude = longitude;
+        this.latitude = latitude;
+        this.averageScoreInNaver = averageScoreInNaver;
+        this.seatSelectionCountInNaver = seatSelectionCountInNaver;
+        this.concentrationSelectionCountInNaver = concentrationSelectionCountInNaver;
     }
-
-    // 나중에 구현
-//    private Double longitude;
-//    private Double latitude;
 
     public void addReview(Review review) {
         review.setCafe(this);
