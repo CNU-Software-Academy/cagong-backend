840c19a21b70eb2fd42ff7a3d548f5a2a8621015
SuJeong Moon <msj1506@naver.com>
2022-10-14 17:42:37 +0900

    feat: 카페 csv 파일 데이터베이스에 저장 (#14)
    * feat: 카페 리스트 파일 불러와서 저장
    
    * feat: 카페 데이터 베이스 컬럼 추가

---
+++ src/main/java/kr/ac/cnu/swacademy/cagong/controller/FileController.java
@@ -0,0 +1,19 @@
+package kr.ac.cnu.swacademy.cagong.controller;
+
+import kr.ac.cnu.swacademy.cagong.service.CafeFileService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class FileController {
+
+    private final CafeFileService cafeFileService;
+
+    @GetMapping("/cafeFile")
+    public String cafeFile() {
+        cafeFileService.save();
+        return "ok";
+    }
+}
