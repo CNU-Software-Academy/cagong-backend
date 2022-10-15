9d365e1b82801d7e139fed2e7b3f7056c9a8b3c2
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-10-15 19:19:34 +0900

    feat : 리뷰 api 컨트롤러에 리뷰 수정 기능 구현

--- src/main/java/kr/ac/cnu/swacademy/cagong/controller/api/ReviewApiController.java
+++ src/main/java/kr/ac/cnu/swacademy/cagong/controller/api/ReviewApiController.java
@@ -1,15 +1,16 @@
 package kr.ac.cnu.swacademy.cagong.controller.api;
 
 import kr.ac.cnu.swacademy.cagong.dto.ReviewSaveRequestDto;
+import kr.ac.cnu.swacademy.cagong.dto.ReviewUpdateRequestDto;
 import kr.ac.cnu.swacademy.cagong.service.CafeService;
 import kr.ac.cnu.swacademy.cagong.service.ReviewService;
 import kr.ac.cnu.swacademy.cagong.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ReviewApiController {
@@ -20,6 +21,13 @@ public class ReviewApiController {
     public ResponseEntity<Long> saveReview(@RequestBody ReviewSaveRequestDto requestDto) {
         Long savedId = reviewService.save(requestDto);
         return ResponseEntity.ok(savedId);
+    }
+
+    @PutMapping("/api/v1/review/{reviewId}")
+    public ResponseEntity<Long> updateReview(@PathVariable Long reviewId, @RequestBody ReviewUpdateRequestDto requestDto) {
+        Long updatedId = reviewService.update(reviewId, requestDto);
+        log.info("{}", requestDto.toString());
+        return ResponseEntity.ok(updatedId);
     }
 
 }
