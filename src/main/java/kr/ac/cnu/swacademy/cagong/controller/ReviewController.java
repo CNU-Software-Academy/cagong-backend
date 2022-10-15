cf31e40ad725c3692ab7f2c1cfdd157336a30831
sanggyunbak2856 <baksanggyun2856@gmail.com>
2022-10-15 16:44:23 +0900

    feat : 리뷰 저장 기능에서 user id, cafe id를 가져오도록 수정

--- src/main/java/kr/ac/cnu/swacademy/cagong/controller/ReviewController.java
+++ src/main/java/kr/ac/cnu/swacademy/cagong/controller/ReviewController.java
@@ -2,8 +2,11 @@ package kr.ac.cnu.swacademy.cagong.controller;
 
 import kr.ac.cnu.swacademy.cagong.dto.ReviewListResponseDto;
 import kr.ac.cnu.swacademy.cagong.dto.ReviewSaveRequestDto;
+import kr.ac.cnu.swacademy.cagong.service.CafeService;
 import kr.ac.cnu.swacademy.cagong.service.ReviewService;
+import kr.ac.cnu.swacademy.cagong.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,9 +18,12 @@ import static ognl.DynamicSubscript.all;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class ReviewController {
 
     private final ReviewService reviewService;
+    private final UserService userService;
+    private final CafeService cafeService;
 
     @GetMapping("/reviews")
     public String reviewList(Model model)
@@ -31,7 +37,11 @@ public class ReviewController {
     public String reviewSave(Model model )
     {
         ReviewSaveRequestDto requestDto = new ReviewSaveRequestDto();
+        List<Long> userIdList = userService.findId();
+        List<Long> cafeIdList = cafeService.findId();
         model.addAttribute("review", requestDto);
+        model.addAttribute("userIdList", userIdList);
+        model.addAttribute("cafeIdList", cafeIdList);
         return "review/saveForm";
     }
 }
