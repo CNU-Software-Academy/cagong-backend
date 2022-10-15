a858592ac192b6046fe2dd12e1d4f404099f37ec
Kim-AYoung <onionlily123@naver.com>
2022-10-15 16:41:43 +0900

    feature: 카페 repository에 전체조회 메소드 추가

--- src/main/java/kr/ac/cnu/swacademy/cagong/repository/CafeRepository.java
+++ src/main/java/kr/ac/cnu/swacademy/cagong/repository/CafeRepository.java
@@ -2,10 +2,15 @@ package kr.ac.cnu.swacademy.cagong.repository;
 
 import kr.ac.cnu.swacademy.cagong.entity.Cafe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CafeRepository extends JpaRepository<Cafe, Long> {
 
     Optional<Cafe> findByName(String name);
+
+    @Query("SELECT c FROM Cafe c ORDER BY c.id DESC")
+    List<Cafe> findAllDesc();
 }
