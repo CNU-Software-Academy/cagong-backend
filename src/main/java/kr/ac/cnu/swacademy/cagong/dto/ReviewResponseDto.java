1911f3103d8eaf5be099541aaa285b9279be0419
pum005 <108645121+pum005@users.noreply.github.com>
2022-10-15 15:26:13 +0900

    Feature/review controller (#13)
    * chore : Getter, Setter 추가
    
    * feat : Review Service 추가
    
    * test : Review Service test 조회, 저장 테스트 추가
    
    * feat : Review Dto 추가
    
    * feat : ReviewSaveRequestDto 추가
    
    * feat : ReviewController 전체 조회, 추가 페이지 추가
    
    * feat : 리뷰 전체 조회 페이지, 리뷰 저장 페이지 추가

---
+++ src/main/java/kr/ac/cnu/swacademy/cagong/dto/ReviewResponseDto.java
@@ -0,0 +1,35 @@
+package kr.ac.cnu.swacademy.cagong.dto;
+
+import kr.ac.cnu.swacademy.cagong.entity.Review;
+
+import java.time.LocalDateTime;
+
+public class ReviewResponseDto {
+
+    private Long id;
+    private Long userId;
+    private Long cafeId;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String content;
+    private int clean;
+    private int seat;
+    private int concentration;
+    private String imageUrl;
+
+
+    public ReviewResponseDto(Review review)
+    {
+        this.id = review.getId();
+        this.userId = review.getUser().getId();
+        this.cafeId = review.getCafe().getId();
+        this.createdAt = review.getCreatedAt();
+        this.updatedAt = review.getUpdatedAt();
+        this.content = review.getContent();
+        this.clean = review.getClean();
+        this.seat = review.getSeat();
+        this.concentration = review.getConcentration();
+        this.imageUrl = review.getImageUrl();
+
+    }
+}
