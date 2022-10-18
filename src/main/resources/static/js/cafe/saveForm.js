83b62fcf6e164ffbd945ee187e65bc9c2e6f6792
Kim-AYoung <onionlily123@naver.com>
2022-10-18 10:28:13 +0900

    feature: 카페 관리자 페이지에 validation 추가

--- src/main/resources/static/js/cafe/saveForm.js
+++ src/main/resources/static/js/cafe/saveForm.js
@@ -20,18 +20,28 @@ let main = {
             totalReviewCount: $('#totalReviewCount').val()
         };
 
-        $.ajax({
-            type: 'POST',
-            url: '/api/cafe',
-            dataType: 'json',
-            contentType:'application/json; charset=utf-8',
-            data: JSON.stringify(data)
-        }).done(function() {
-            alert('카페가 추가되었습니다.');
-            location.href = "/admin/cafes";
-        }).fail(function (error) {
-            console.log(JSON.stringify(error));
-        });
+        if(data.name === "") {
+            alert("카페 이름을 입력해주세요.")
+        } else if(data.address === "") {
+            alert("카페 주소를 입력해주세요.")
+        } else if(data.averageScore < 0 || data.averageScore > 5) {
+            alert("Average Score의 범위는 0부터 5까지 입니다.")
+        } else if(data.studyScore < 0 || data.studyScore > 1) {
+            alert("Study Score의 범위는 0부터 1까지 입니다.")
+        } else {
+            $.ajax({
+                type: 'POST',
+                url: '/api/cafe',
+                dataType: 'json',
+                contentType:'application/json; charset=utf-8',
+                data: JSON.stringify(data)
+            }).done(function() {
+                alert('카페가 추가되었습니다.');
+                location.href = "/admin/cafes";
+            }).fail(function (error) {
+                console.log(JSON.stringify(error));
+            });
+        }
     },
 };
 main.init();
