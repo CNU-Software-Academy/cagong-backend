36cfee97ee10c672547f8bd4d0b2e4eb852a3578
moonstal1506 <moonstal1506@gmail.com>
2022-10-25 16:32:46 +0900

    feat: 중복 회원 검증

--- src/main/java/kr/ac/cnu/swacademy/cagong/service/UserService.java
+++ src/main/java/kr/ac/cnu/swacademy/cagong/service/UserService.java
@@ -5,8 +5,10 @@ import kr.ac.cnu.swacademy.cagong.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
+@Transactional
 @RequiredArgsConstructor
 @Service
 public class UserService {
@@ -14,11 +16,16 @@ public class UserService {
     private final UserRepository userRepository;
 
     public String join(User user) {
-        userRepository.save(User.builder()
-                .username(user.getUsername())
-                .password(user.getPassword())
-                .build());
+        validateDuplicateUser(user);
+        userRepository.save(user);
         return "Success";
+    }
+
+    private void validateDuplicateUser(User user){
+        User findUser = userRepository.findByUsername(user.getUsername());
+        if(findUser != null){
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
     }
 
     public String login(String username, String password) {
---
+++ src/test/java/kr/ac/cnu/swacademy/cagong/service/UserServiceTest.java
@@ -0,0 +1,46 @@
+package kr.ac.cnu.swacademy.cagong.service;
+
+import kr.ac.cnu.swacademy.cagong.dto.UserFormDto;
+import kr.ac.cnu.swacademy.cagong.entity.User;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+@SpringBootTest
+@Transactional
+class UserServiceTest {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    public User createUser() {
+        UserFormDto userFormDto = new UserFormDto();
+        userFormDto.setEmail("test@email.com");
+        userFormDto.setName("홍길동");
+        userFormDto.setPassword("1234");
+        return User.createUser(userFormDto, passwordEncoder);
+    }
+
+    @Test
+    @DisplayName("중복 회원 가입 테스트")
+    public void saveDuplicateUserTest() {
+        User user1 = createUser();
+        User user2 = createUser();
+        userService.join(user1);
+        Throwable e = assertThrows(IllegalStateException.class, () -> {
+            userService.join(user2);
+        });
+        assertThat(e.getMessage()).isEqualTo("이미 가입된 회원입니다.");
+    }
+
+}
