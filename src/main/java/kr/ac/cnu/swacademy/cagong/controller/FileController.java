af8a98b690c8d9ab4a171eca79023807d83786f1
SuJeong Moon <msj1506@naver.com>
2022-10-26 00:43:50 +0900

    feat: admin만 공지사항 수정할 수 있도록 변경 (#36)

--- src/main/java/kr/ac/cnu/swacademy/cagong/controller/FileController.java
+++ src/main/java/kr/ac/cnu/swacademy/cagong/controller/FileController.java
@@ -11,7 +11,7 @@ public class FileController {
 
     private final CafeFileService cafeFileService;
 
-    @GetMapping("/cafeFile")
+    @GetMapping("/admin/cafeFile")
     public String cafeFile() {
         cafeFileService.save();
         return "ok";
