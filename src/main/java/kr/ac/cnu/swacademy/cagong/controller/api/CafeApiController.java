01d9d669ba52329bdd7557c1af4c9a3a5435fa00
KIMA <57066971+Kim-AYoung@users.noreply.github.com>
2022-10-28 22:26:54 +0900

    feat: 카페 정렬 검색 구현 (#60)
    * feat: 카페 정렬 검색 API 구현
    
    * style : 카페 정렬버튼 추가
    
    * feat : 카페 정렬 검색 API에 대한 서비스, repo 구현
    
    * feat : 카페 키워드없이 정렬 검색할 때 서비스 구현

--- src/main/java/kr/ac/cnu/swacademy/cagong/controller/api/CafeApiController.java
+++ src/main/java/kr/ac/cnu/swacademy/cagong/controller/api/CafeApiController.java
@@ -6,11 +6,21 @@ import kr.ac.cnu.swacademy.cagong.service.CafeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 public class CafeApiController {
 
     private final CafeService cafeService;
+
+    @GetMapping("/api/cafes")
+    public List<CafeResponseDto> findAll(@RequestParam(name = "sortBy", defaultValue = "", required = false) String sortBy){
+        if (!List.of("", "average_score", "average_price", "study_score").contains(sortBy)) {
+            throw new IllegalArgumentException(sortBy + "은 잘못된 정렬 방식입니다.");
+        }
+        return cafeService.findAllDesc(sortBy);
+    }
 
     @PostMapping("/api/cafe")
     public long save(@RequestBody CafeSaveRequestDto requestDto) {
--- src/main/resources/static/js/cafe/cafeList.js
+++ src/main/resources/static/js/cafe/cafeList.js
@@ -1,17 +1,55 @@
 let main = {
-    init : function () {
+    init: function () {
         let _this = this;
         $('#btn-search').on('click', function () {
             _this.search();
         });
+        $('#btn-sort-by-average-score').on('click', function () {
+            _this.sortBy("average_score");
+        });
+        $('#btn-sort-by-average-price').on('click', function () {
+            _this.sortBy("average_price");
+        });
+        $('#btn-sort-by-study-score').on('click', function () {
+            _this.sortBy("study_score");
+        });
     },
-    search : function () {
+    search: function () {
         let keyword = $('#keyword').val();
-        if(keyword === "") {
+        if (keyword === "") {
             alert("키워드를 입력해주세요.");
         } else {
-            location.href = (location.href + "/search/keyword/" + keyword);
+            location.href = (getHostname(location.href) + "/cafes/search/keyword/" + keyword);
         }
     },
+    sortBy: function (attribute) {
+        let href = location.href;
+        if (isExistSorting(href)) {
+            href = eraseSorting(href);
+        }
+        if (isKeywordSearch(href)) {
+            location.href = href + "?sortBy=" + attribute;
+        } else {
+            location.href = (getHostname(href) + "/cafes/search/keyword/all?sortBy=" + attribute);
+        }
+    }
 };
 main.init();
+
+function isKeywordSearch(href) {
+    return href.indexOf("/search/keyword") !== -1;
+}
+
+function isExistSorting(href) {
+    return href.indexOf("?sortBy=") !== -1;
+}
+
+function getHostname(href) {
+    let cafesIndex = href.indexOf("/cafes");
+    return href.substring(0, cafesIndex);
+}
+
+function eraseSorting(href) {
+    let cafesIndex = href.indexOf("?sortBy=");
+    return href.substring(0, cafesIndex);
+}
