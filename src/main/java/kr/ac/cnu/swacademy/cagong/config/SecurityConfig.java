b12ac91ef0af577074e418dadff4b6ccaa5d20d6
Kim-AYoung <onionlily123@naver.com>
2022-10-28 15:11:53 +0900

    fix: login 템플릿 url 수정

--- src/main/java/kr/ac/cnu/swacademy/cagong/config/SecurityConfig.java
+++ src/main/java/kr/ac/cnu/swacademy/cagong/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package kr.ac.cnu.swacademy.cagong.config;
 
 import kr.ac.cnu.swacademy.cagong.service.UserService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,30 +13,24 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 @EnableWebSecurity
 @Configuration
-@Slf4j
 public class SecurityConfig {
 
     private final UserService userService;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
-        try {
-            return http.csrf().disable()
-                    .authorizeRequests()
-                    .mvcMatchers("/admin/**").hasRole("ADMIN")
-                    .anyRequest()
-                    .permitAll()
-                    .and()
-                    .formLogin()
-                    .loginPage("/users/login")
-                    .loginProcessingUrl("/login")
-                    .defaultSuccessUrl("/")
-                    .failureUrl("/users/login/error")
-                    .and().build();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf().disable()
+                .authorizeRequests()
+                .mvcMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest()
+                .permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/users/login")
+                .loginProcessingUrl("/login")
+                .defaultSuccessUrl("/")
+                .failureUrl("/users/login/error")
+                .and().build();
     }
 
     @Bean
