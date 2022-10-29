9d03fd51e2d492bb713f9f08b354dcbe551f16e4
moonstal1506 <moonstal1506@gmail.com>
2022-10-29 17:53:05 +0900

    fix: @Param 에러 해결

--- src/main/java/kr/ac/cnu/swacademy/cagong/repository/ReviewRepository.java
+++ src/main/java/kr/ac/cnu/swacademy/cagong/repository/ReviewRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,7 +15,7 @@ import java.util.List;
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query("SELECT r FROM Review r where r.cafe.id = :id")
-    public List<Review> findReviewsByCafe(Long id);
+    public List<Review> findReviewsByCafe(@Param("id") Long id);
 
     @Query("select r from Review r order by r.id desc")
     Page<Review> findAllDesc(Pageable pageable);
